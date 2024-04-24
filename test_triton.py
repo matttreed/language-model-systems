930f4f85f3f8d734440d5d8cbcb97ef4d4440ccf
Matt Reed <matthewreed02@gmail.com>
2024-04-24 12:55:02 -0700

    cuda

--- test_triton.py
+++ test_triton.py
@@ -3,8 +3,8 @@ from cs336_systems.rms_kernel import WeightedSumFunc
 
 
 def main():
-    x = torch.randn(8,32,64)
-    w = torch.randn(64)
+    x = torch.randn(8,32,64).cuda()
+    w = torch.randn(64).cuda()
     weighted_sum_func = WeightedSumFunc.apply
     triton_y = weighted_sum_func(x, w)
     python_y = (w * x).sum(axis=-1)
