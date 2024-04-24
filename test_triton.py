781eba7c6cc648d78437f8ed5d2dbe79e2745617
Matt Reed <matthewreed02@gmail.com>
2024-04-24 13:10:13 -0700

    erge

--- test_triton.py
+++ test_triton.py
@@ -4,13 +4,16 @@ from cs336_systems.functions import RMS_Norm_Func_Python
 
 
 def main():
-    x = torch.randn(8,32,64).cuda()
-    w = torch.randn(64).cuda()
+    x = torch.randn(4,2,3).cuda()
+    w = torch.randn(3).cuda()
     weighted_sum_func = WeightedSumFunc_Triton.apply
     triton_y = weighted_sum_func(x, w)
     python_y = (w * x).sum(axis=-1)
 
     assert torch.allclose(triton_y, python_y), "Output mismatch"
+
+    print("TRITON: ", triton_y)
+    print("PYTHON: ", python_y)
 
 if __name__ == "__main__":
     main()
