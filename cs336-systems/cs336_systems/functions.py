3bbb98d4ef9bcc29ada5a53d93d4d2fb85b0b088
Matty Reed <matthewreed02@gmail.com>
2024-04-26 11:33:35 -0700

    python grad passes

--- cs336-systems/cs336_systems/functions.py
+++ cs336-systems/cs336_systems/functions.py
@@ -1,10 +1,31 @@
 import torch
 
 def rmsnorm_jvp_g(dL_drms, x, g):
-    return g
+    rms = torch.sqrt(torch.mean(x**2, dim=-1, keepdim=True) + 1e-5)
+    num_dims = x.dim()
+    dims_to_sum = list(range(num_dims - 1))
+    result = torch.sum(dL_drms * x / rms, dim=dims_to_sum)
+    return result
 
 def rmsnorm_jvp_x(dL_drms, x, g):
-    return x
+    eps = 1e-5
+    d = x.shape[-1]
+
+    # Compute RMS and normalized x
+    rms = torch.sqrt(torch.mean(x**2, dim=-1, keepdim=True) + eps)
+
+    # Gradient with respect to x due to scaling by gain
+    grad_x_scaling = (dL_drms * g) / rms
+
+    # Additional gradient due to normalization's impact on RMS
+    grad_x_additional = -(
+        x * torch.mean(dL_drms * x * g, dim=-1, keepdim=True) / (rms**3)
+    )
+
+    print(x.shape, grad_x_scaling.shape, grad_x_additional.shape)
+
+    # Total gradient with respect to x
+    return grad_x_scaling + grad_x_additional
 
 class RMS_Norm_Func_Python(torch.autograd.Function):
     @staticmethod
