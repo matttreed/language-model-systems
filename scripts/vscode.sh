2f4387e2ee5e8d0804a65feaf5e61a97cf9a4cdd
Matt Reed <matthewreed02@gmail.com>
2024-04-25 17:40:08 -0700

    vscode script

---
+++ scripts/vscode.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=vscode
+#SBATCH --partition=batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=06:00:00
+#SBATCH --output=scripts/logs/vscode_%j.out
+#SBATCH --error=scripts/logs/vscode_%j.err
+#SBATCH --mem=10G
+#SBATCH --nodelist=ad12a3ca-01
+
+--nodelist=ad12a3ca-01
