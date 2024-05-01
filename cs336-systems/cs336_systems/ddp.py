d83a79315bc4ec3efbbe6332e566ce14f31861ff
Matty Reed <matthewreed02@gmail.com>
2024-04-30 22:58:01 -0700

    small changes

--- cs336-systems/cs336_systems/ddp.py
+++ cs336-systems/cs336_systems/ddp.py
@@ -6,20 +6,20 @@ from cs336_basics.configs.config import Config
 from cs336_basics.model.transformer import Transformer
 from cs336_basics.training.optimizer import AdamW
 from cs336_basics.model.util import crossEntropyLoss, load_model, save_model, get_batch, log_validation_loss, log, build_model
+from torch._utils import _flatten_dense_tensors, _unflatten_dense_tensors
 import torch
 import numpy as np
 import torch.nn as nn
 from copy import deepcopy
 
 class DDP_Individual_Parameters(torch.nn.Module):
-    def __init__(self, module: torch.nn.Module, backend="nccl") -> None:
+    def __init__(self, module: torch.nn.Module) -> None:
         super(DDP_Individual_Parameters, self).__init__()
 
         if not dist.is_initialized():
             raise RuntimeError("Distributed package is not initialized")
 
         self.module = module
-        self.backend = backend
 
         self.rank = dist.get_rank()
         self.world_size = dist.get_world_size()
@@ -50,6 +50,43 @@ class DDP_Individual_Parameters(torch.nn.Module):
         self.handles.clear()
 
 class DDP_Bucketed(torch.nn.Module):
+    def __init__(self, module: torch.nn.Module, bucket_size_mb: float) -> None:
+        super(DDP_Bucketed, self).__init__()
+
+        if not dist.is_initialized():
+            raise RuntimeError("Distributed package is not initialized")
+
+        self.module = module
+
+        self.rank = dist.get_rank()
+        self.world_size = dist.get_world_size()
+
+        self.handles = []
+
+        def register_post_accumulate_grad_hook(param):
+            grad = param.grad
+            if grad is not None:
+                grad /= self.world_size
+                handle = dist.all_reduce(grad, op=torch.distributed.ReduceOp.SUM, async_op=True)
+                self.handles.append(handle)
+                
+
+        for param in self.module.parameters():
+            if param.requires_grad:
+                param.register_post_accumulate_grad_hook(register_post_accumulate_grad_hook)
+            dist.broadcast(param.data, src=0)
+    
+    def forward(self, *inputs, **kwargs):
+        y = self.module(*inputs, **kwargs)
+        return y
+
+    def finish_gradient_synchronization(self):
+
+        for handle in self.handles:
+            handle.wait()
+        self.handles.clear()
+
+class DDP_Bucketed_2(torch.nn.Module):
     def __init__(self, module: torch.nn.Module, bucket_size_mb: float, backend="nccl") -> None:
         super(DDP_Bucketed, self).__init__()
 
@@ -86,7 +123,6 @@ class DDP_Bucketed(torch.nn.Module):
             self.buckets.append(current_bucket)
 
         self.bucket_ready = [0 for _ in range(len(self.buckets))]
-        self.bucket_concat = [torch.empty((sum([param.numel() for param in bucket]))) for bucket in self.buckets]
 
         for bucket_index, bucket in enumerate(self.buckets):
             for param in bucket:
@@ -100,12 +136,13 @@ class DDP_Bucketed(torch.nn.Module):
         return grad
     
     def communicate_bucket(self, bucket_index):
-        flat_grads = [param.grad.view(-1) for param in self.buckets[bucket_index] if param.grad is not None]  # Flatten each gradient
-        if flat_grads:
-            self.bucket_concat[bucket_index] = torch.cat(flat_grads)
-            handle = dist.all_reduce(self.bucket_concat[bucket_index], op=dist.ReduceOp.SUM, async_op=True)
-            self.handles.append((handle, bucket_index))
-            self.bucket_ready[bucket_index] = 0
+        grads = [param.grad for param in self.buckets[bucket_index] if param.grad is not None]
+        if grads:
+            dense = _flatten_dense_tensors(grads)
+            handle = dist.all_reduce(dense, op=dist.ReduceOp.SUM, async_op=True)
+            self.handles.append((handle, dense, bucket_index))
+        
+        self.bucket_ready[bucket_index] = 0
     
     def forward(self, *inputs, **kwargs):
         y = self.module(*inputs, **kwargs)
@@ -113,12 +150,10 @@ class DDP_Bucketed(torch.nn.Module):
 
     def finish_gradient_synchronization(self):
 
-        for (handle, bucket_index) in self.handles:
+        for (handle, dense, bucket_index) in self.handles:
+            grads = [param.grad for param in self.buckets[bucket_index] if param.grad is not None]
             handle.wait()
-            offset = 0
-            for param in self.buckets[bucket_index]:
-                if param.grad is not None:
-                    numel = param.numel()
-                    param.grad.data.copy_(self.bucket_concat[bucket_index][offset:offset+numel].view_as(param.grad))
-                    offset += numel
+            unflattened_tensors = _unflatten_dense_tensors(dense, grads)
+            for param, unflattened in zip(self.buckets[bucket_index], unflattened_tensors):
+                param.data = unflattened
         self.handles.clear()
